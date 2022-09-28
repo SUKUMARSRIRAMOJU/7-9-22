84cd0c4f5985f3895750c1495bddfc2b639f1490
Sukumar Sriramoju <79364589+SUKUMARSRIRAMOJU@users.noreply.github.com>
2022-09-28 11:43:36 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,51 +1,16 @@
-import React, {useState,useEffect} from "react";
-import "./App.css";
-import Axios from 'axios'
-function App()
+import ReactDOM from 'react-dom';
+
+function App() 
 {
-const[sname,setStudentName]= useState("");
-const[tech,setTechnology]= useState("");
-const[status,setStatus]= useState("");
-const submitReview=()=>
-{
-Axios.post('http://localhost:9000/aliens',
-{name:sname,
-tech:tech,
-sub:status}).then(()=>
-{
-alert("success");
-});
-};
-return (
-<div className="App">
-<h1>CRUD Application Demo</h1>
-<div className="information">
-<label><b>Student Name</b></label>
-<input
-type="text"
-name="sname"
-onChange={(e)=>{
-setStudentName(e.target.value);
-}}
-required/>
-<label><b>Technology</b></label>
-<input
-type="text"
-name="tech"
-onChange={(e)=>{
-setTechnology(e.target.value);
-}}
-required/>
-<label><b>Status</b></label>
-<input
-type="text"
-name="status"
-onChange={(e)=>{
-setStatus(e.target.value);
-}}
-required/>
-<button onClick={submitReview}><b>Submit</b></button>
-</div>
-</div>);
+ const element= 
+ (
+    <div >
+      <h1>Welcome to CBIT</h1>
+      <h1>Now the Time is</h1>
+      <p>{new Date().toLocaleTimeString()}</p>
+    </div>
+  );
+  ReactDOM.render(element, document.getElementById('root'));
 }
+setInterval(App, 1000);
 export default App;
